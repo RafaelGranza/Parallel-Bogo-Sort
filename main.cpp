d51825e281714ad02a3115fa19b61cfc78d58400
RafaelGranza <rafaelgranzademello@gmail.com>
2021-03-24 21:41:13 -0300

    Update

--- main.cpp
+++ main.cpp
@@ -9,9 +9,14 @@ int main(int argc, char const *argv[])
     // vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 1m 20s
     vector<int> vec = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 1,5s
     // vector<int> vec = {5, 4, 3, 2, 1, 0}; // Seq: 0s
-    vec = omp_bogo(vec);
+    
+    vec = seq_bogo(vec);
+    // vec = omp_bogo(vec); // Falta programar CERTO
+    // vec = cuda_bogo(vec);  // Nem comecei ainda
+
 
     for(auto i: vec) cout << i << " ";
     cout << endl;
+    
     return 0;
 }
--- src/omp_bogo.cpp
+++ src/omp_bogo.cpp
@@ -1,29 +1,21 @@
 #include <bits/stdc++.h>
 #include <omp.h>
-#include "seq_bogo.cpp"
 using namespace std;
 
-bool omp_is_ordered(vector<int>& vec){
-     for(auto& v: vec) if(is_ordered(v)) return true;
+bool omp_is_ordered(vector<int> vec){
+    return is_sorted(vec.begin(), vec.end());
 }
 
-vector<int> omp_ordered(vector<vector<int>>& vec){
-    for(auto& v: vec) if(is_ordered(v)) return v;
+void omp_next(vector<int> vec){
+    random_shuffle(vec.begin(), vec.end());
 }
 
-void omp_next(vector<vector<int>>& vec){
-    for(auto& v: vec) random_shuffle(v.begin(), v.end());
-}
-
-vector<int> omp_bogo(vector<int> vec){
+vector<int> omp_bogo(vector<int>& vec){
 
     // Talvez criar várias cópias e rodar em diferentes threads
-
-    vector<vector<int>> vecs(10, vec);
-
     #pragma omp parallel
-    while(not omp_is_ordered(vecs)){
-        omp_next(vecs);
+    while(not omp_is_ordered(vec)){
+        omp_next(vec);
     }
     return vec;
 }
