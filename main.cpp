2b91f9b775cae0d970d1343593e0b3cacf42aef3
RafaelGranza <rafaelgranzademello@gmail.com>
2021-03-29 17:34:16 -0300

    Finishing OMP

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // vector<int> vec = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 1m 25s | OMP: ~ 1m 0s
-    // vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 18s | OMP: ~ 1m 0s
-    // vector<int> vec = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 0,75s | OMP: ~ 0,75s
-    vector<int> vec = {7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 0,2s | OMP: ~ 0,04s
+    // vector<int> vec = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 2m 00s | OMP: ~ 7,46s
+    // vector<int> vec = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 18s | OMP: ~  2,3s
+    vector<int> vec = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 0,75s | OMP: ~ 0,35s
+    // vector<int> vec = {7, 6, 5, 4, 3, 2, 1, 0}; // Seq: ~ 0,2s | OMP: ~ 0,04s
     // vector<int> vec = {5, 4, 3, 2, 1, 0}; // Seq: 0,01s | OMP: 0,1s
     
-    // vec = seq_bogo(vec);
-    vec = omp_bogo(vec, 12); // Falta deixar Rápido
+    vec = seq_bogo(vec); // PRONTO
+    // vec = omp_bogo(vec, 4); // PRONTO
     // vec = cuda_bogo(vec);  // Nem comecei ainda
 
     for(auto i: vec) cout << i << " ";
--- src/omp_bogo.cpp
+++ src/omp_bogo.cpp
@@ -3,33 +3,30 @@
 #include <omp.h>
 using namespace std;
 
-vector<int> omp_bogo(vector<int>& vec, int size = 12){
+vector<int> omp_bogo(vector<int>& vec, int size = 2){
 
-    // Talvez criar várias cópias e rodar em diferentes threads
     vector<vector<int>> vecs(size, vec);
     vector<mt19937> engine(size);
     bool done = false;
      
-    #pragma omp parallel for shared(vecs, done)
+    #pragma omp parallel for shared(vecs, engine)
     for(int i = 0; i < size; i++){
         engine[i] = mt19937(omp_get_thread_num()*(i+1));
         next(vecs[i], engine[i]);
-        if(is_ordered(vecs[i])) {
-            done = true;
-            vec = vecs[i];
-        }
-    }
-
-    while(!done){
-        #pragma omp parallel for  shared(vecs, done)
-        for(int i = 0; i < size; i++){
-            if(is_ordered(vecs[i])) {
-                done = true;
-                vec = vecs[i];
-            }
-            next(vecs[i], engine[i]);
-        }
     }
     
+    #pragma omp parallel num_threads(size) shared(vecs, engine, done)
+    {
+        while(not is_ordered(vecs[omp_get_thread_num()]) && !done){
+            next(vecs[omp_get_thread_num()], engine[omp_get_thread_num()]);
+        }
+        done = true; 
+    }
+
+    for(int i = 0; i < size; i++){
+        if(is_ordered(vecs[i])){
+            return vecs[i];
+        }
+    }
     return vec;
 }
