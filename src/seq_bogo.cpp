55aff789b741f2bfd5bf058ba603798590cfe0fd
RafaelGranza <rafaelgranzademello@gmail.com>
2021-04-06 15:57:15 -0300

    Format fix

--- src/seq_bogo.cpp
+++ src/seq_bogo.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-vector<int> seq_bogo(vector<int> vec, int s = 0){
+vector<int> seq_bogo(vector<int> vec, int s = 0)
+{
     unsigned seed = time(0) * (s+1);
     mt19937 engine(seed);
 
-    while(not is_ordered(vec)){
+    while(not is_ordered(vec))
+    {
         next(vec, engine);
     }
     return vec;
--- src/tools.cpp
+++ src/tools.cpp
@@ -2,13 +2,16 @@
 #pragma once
 using namespace std;
 
-bool is_ordered(vector<int>& vec){
+bool is_ordered(vector<int>& vec)
+{
     return is_sorted(vec.begin(), vec.end());
 }
 
-void next(vector<int>& vec, mt19937& r){
+void next(vector<int>& vec, mt19937& r)
+{
     int n = vec.size();
-    for (int i = 0; i < n - 1; i++){
+    for (int i = 0; i < n - 1; i++)
+    {
         int range = n-1 - i + 1;
         unsigned int j = r() % range + i;
         int t = vec[j];
